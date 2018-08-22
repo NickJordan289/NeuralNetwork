09d3264ff7ae88df44b114e1534421bce4db70e8
Nick Jordan <35259816+NickJordan289@users.noreply.github.com>
2018-08-22 17:46:05 +0800

    Updated batch training example to use newly implemented function

--- examples/BatchTraining.cpp
+++ examples/BatchTraining.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <time.h>
 #include <string>
 #include <fstream>
@@ -9,77 +10,112 @@
 #include "Matrix.h"
 #include "ExtraFuncs.h"
 
-std::vector<std::vector<double>> readCSV(std::string filepath) {
-	std::ifstream file(filepath, std::ifstream::in);
-	std::vector<std::vector<double>> data;
+namespace DataSet {
+	/*
+		Reads the given file seperating into lines and values
+		seperates lines using the '\n' delimiter 
+		and seperates values using the given delim char (default ',')
+	*/
+	std::vector<std::vector<double>> read(std::string filepath, char delim=',') {
+		std::ifstream file(filepath, std::ifstream::in);
+		std::vector<std::vector<double>> data;
 
-	while (file.good()) {
-		std::string line;
-		std::getline(file, line);
+		while (file.good()) {
+			std::string line;
+			std::getline(file, line);
 
-		std::stringstream values(line);
+			std::stringstream values(line);
 
-		std::vector<double> valsInLine;
-		while (values.good()) {
-			std::string newValue;
-			std::getline(values, newValue, ',');
-			valsInLine.push_back(std::stod(newValue));
+			std::vector<double> valsInLine;
+			while (values.good()) {
+				std::string newValue;
+				std::getline(values, newValue, delim);
+				valsInLine.push_back(std::stod(newValue));
+			}
+			data.push_back(valsInLine);
 		}
-		data.push_back(valsInLine);
+		file.close();
+		return data;
 	}
-	file.close();
-	return data;
-}
 
-std::vector<double> encodeLabel(int val, int possiblities) {
-	std::vector<double> label;
-	for (int i = 0; i <possiblities; i++) {
-		label.push_back(0.0);
-	}
-	label[val] = 1.0;
-	return label;
-}
-
-std::vector<std::vector<double>> encodeLabels(std::vector<std::vector<double>>& vals, int possiblities) {
-	std::vector<std::vector<double>> labels;
-	for (std::vector<double> a : vals) {
-		for (double val : a) {
-			labels.push_back(encodeLabel(val, possiblities));
+	/*
+		Converts integer to a vector filled with zeros
+		eg. val=1, possibilities=3
+		[0, 1, 0], index val is 1 where all other entries are 0
+	*/
+	std::vector<double> encodeLabel(int val, int possiblities) {
+		std::vector<double> label;
+		for (int i = 0; i < possiblities; i++) {
+			label.push_back(0.0);
 		}
+		label[val] = 1.0;
+		return label;
 	}
-	vals = labels;
-	return labels;
-}
 
-int getOutputCount(std::vector<std::vector<double>> labels) {
-	std::sort(labels.begin(), labels.end());
-	int max = -1;
-	int count = 0;
-	for (std::vector<double> line : labels) {
-		for (double value : line) {
-			if (value > max) {
-				max = value;
-				count++;
+	/*
+		Encodes a dataset of integers into their respective output possibility chance
+		see above function for what a better explanation of what is actually happening
+
+		example. vals = {0, 1, 2, 0}
+		returns {{1,0,0},{0,1,0},{0,0,1},{1,0,0}}
+	*/
+	std::vector<std::vector<double>> encodeLabels(std::vector<std::vector<double>>& vals, int possiblities) {
+		std::vector<std::vector<double>> labels;
+		for (std::vector<double> a : vals) {
+			for (double val : a) {
+				labels.push_back(encodeLabel(val, possiblities));
 			}
 		}
+		vals = labels;
+		return labels;
 	}
-	return count;
-}
 
-template <typename T>
-void split(std::vector<T> a, std::vector<T>& outB, std::vector<T>& outC) {
-	for (int i = 0; i < ceil(a.size() / 2.f); i++)
-		outB.push_back(a[i]);
-	for (int i = ceil(a.size() / 2.f); i < a.size(); i++)
-		outC.push_back(a[i]);
+	/*
+		Hacky solution to determining the amount of unique outputs in a given label vector
+		Sorts so the values go from min to max then counts only numbers it hasn't seen
+	*/
+	int getOutputCount(std::vector<std::vector<double>> labels) {
+		std::sort(labels.begin(), labels.end());
+		int max = -1;
+		int count = 0;
+		for (std::vector<double> line : labels) {
+			for (double value : line) {
+				if (value > max) {
+					max = value;
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	/*
+		Splits a given vector into two vectors
+		Split ratio is given by ratioA and ratioB
+		default split ratio of 1/2
+	*/
+	template <typename T>
+	void split(std::vector<T> a, std::vector<T>& outB, std::vector<T>& outC, int ratioA = 1, int ratioB = 1) {
+		int totalRatio = ratioA + ratioB;
+		float percent = (float)ratioA / (float)totalRatio;
+		for (int i = 0; i < ceil(a.size() * percent); i++)
+			outB.push_back(a[i]);
+		for (int i = ceil(a.size() * percent); i < a.size(); i++)
+			outC.push_back(a[i]);
+	}
 }
 
 int main() {
-	srand(static_cast<unsigned int>(time(NULL)));
+	// Training data setup
+	std::vector<std::vector<double>> irisData = DataSet::read("iris-data.csv", ',');
+	std::vector<std::vector<double>> irisDataLabels = DataSet::read("iris-labels.csv", ',');
 
-	/*// Training data setup
-	std::vector<std::vector<double>> irisData = readCSV("iris-data.csv");
-	std::vector<std::vector<double>> irisDataLabels = readCSV("iris-labels.csv");
+	// initial fixed seed shuffle
+	// doing this because the input data set is sorted
+	srand(1); 
+	std::random_shuffle(std::begin(irisData), std::end(irisData));
+	srand(1);
+	std::random_shuffle(std::begin(irisDataLabels), std::end(irisDataLabels));
 
 	std::vector<std::vector<double>> trainingData;
 	std::vector<std::vector<double>> trainingDataLabels;
@@ -87,11 +123,13 @@ int main() {
 	std::vector<std::vector<double>> testingData;
 	std::vector<std::vector<double>> testingDataLabels;
 
-	split(irisData, trainingData, testingData);
-	split(irisDataLabels, trainingDataLabels, testingDataLabels);
+	// split data 2/3 to training, 1/3 to testing
+	DataSet::split(irisData, trainingData, testingData, 2, 1);
+	DataSet::split(irisDataLabels, trainingDataLabels, testingDataLabels, 2, 1);
 
-	encodeLabels(trainingDataLabels, getOutputCount(trainingDataLabels));
-	encodeLabels(testingDataLabels, getOutputCount(testingDataLabels));
+	// convert from 0, 1, 2 to [1,0,0], [0,1,0], [0,0,1]
+	DataSet::encodeLabels(trainingDataLabels, DataSet::getOutputCount(trainingDataLabels));
+	DataSet::encodeLabels(testingDataLabels, DataSet::getOutputCount(testingDataLabels));
 
 	std::cout << "Training data size: " << trainingData.size() << std::endl;
 	std::cout << "Training labels size: " << trainingDataLabels.size() << std::endl;
@@ -100,85 +138,49 @@ int main() {
 	std::cout << "Testing data size: " << testingData.size() << std::endl;
 	std::cout << "Testing labels size: " << testingDataLabels.size() << std::endl;
 	std::cout << "Testing inputs: " << testingData[0].size() << std::endl;
-	std::cout << "Testing outputs: " << testingDataLabels[0].size() << std::endl;*/
+	std::cout << "Testing outputs: " << testingDataLabels[0].size() << std::endl;
+	std::cout << std::endl;
 
-	int SAMPLES = 1050;
-	int BATCH_SIZE = 100;
-	int ITERATIONS = 10;
+	srand(static_cast<unsigned int>(time(NULL)));
 
-	for (int i = 0; i < ITERATIONS; i++) {
-		for (int j = 0; j < SAMPLES / BATCH_SIZE; j++) {
-			std::cout << "----" << j * BATCH_SIZE << "->" << (1 + j) * BATCH_SIZE - 1 << "----" << std::endl;
-			for (int k = j * BATCH_SIZE; k < (1 + j) * BATCH_SIZE; k++) {
-				std::cout << k << std::endl;
-			}
-		}
-		if (SAMPLES % BATCH_SIZE != 0) { // remainder batch
-			std::cout << "----" << ((SAMPLES / BATCH_SIZE)*BATCH_SIZE) << "->" << ((SAMPLES / BATCH_SIZE)*BATCH_SIZE) + (SAMPLES % BATCH_SIZE) - 1 << "----" << std::endl;
-			for (int j = ((SAMPLES / BATCH_SIZE)*BATCH_SIZE); j < ((SAMPLES / BATCH_SIZE)*BATCH_SIZE) + (SAMPLES % BATCH_SIZE); j++) {
-				std::cout << j << std::endl;
-			}
-		}
-	}
-	
-/*
 	// Neural Net Parameters
-	int hiddenLayers = 8;
-	int trainingIterations = 100000;
-	double trainingRate = 0.1;
+	int hiddenLayers = 3;
 	doubleFunction activationFunc = NeuralNetwork::sigmoid;
 	doubleFunction derivativeFunc = NeuralNetwork::sigmoid_d;
-
+	matrixFunction classifierFunc = NeuralNetwork::softmax;
+	
+	// Training Parameters
+	double trainingRate = 0.05;
+	int BATCH_SIZE = 10;
+	int ITERATIONS = 200;
+	
 	// constructor sets up input and output size based on the data
-	//NeuralNetwork nn(trainingData[0].size(), hiddenLayers, trainingDataLabels[0].size(), trainingRate, activationFunc, derivativeFunc);
-	NeuralNetwork nn(4, hiddenLayers, 3, trainingRate, activationFunc, derivativeFunc);
+	NeuralNetwork nn(trainingData[0].size(), hiddenLayers, trainingDataLabels[0].size(), trainingRate, activationFunc, derivativeFunc, classifierFunc);
 
-	// training loop
-	for (int i = 0; i < trainingIterations; i++) {
-		int randomIndex = rand() % trainingData.size();
-		nn.train(trainingData[randomIndex], trainingDataLabels[randomIndex]);
-	}
+	// simple batch train that uses mse to calculate biases
+	nn.batchTrain(trainingData, trainingDataLabels, ITERATIONS, BATCH_SIZE);
 
 	// simple validation
 	int correct = 0;
 	for (int i = 0; i < testingData.size(); i++) {
-		std::vector<double> output = nn.predict(testingData[i]).toVector(); // round each element in the matrix to either 0 or 1
-		std::vector<double> expectedOutput = testingDataLabels[i];
-		
-		double ahighest = -1;
-		double ahighestIndex = -1;
-		for (int i = 0; i < output.size(); i++) {
-			if (output[i] > ahighest) {
-				ahighest = output[i];
-				ahighestIndex = i;
-			}
-		}
+		Matrix output = nn.predict(testingData[i]).map(round); // predicts then rounds output
+		Matrix expectedOutput = Matrix::fromVector(testingDataLabels[i]);
 
-		double bhighest = -1;
-		double bhighestIndex = -1;
-		for (int i = 0; i < expectedOutput.size(); i++) {
-			if (expectedOutput[i] > bhighest) {
-				bhighest = expectedOutput[i];
-				bhighestIndex = i;
-			}
-		}
-
-		if (ahighestIndex == bhighestIndex) {
+		if (output == expectedOutput) {
 			correct++;
 		}
-		//std::cout << (ahighestIndex == bhighestIndex ? "Correct!" : "Incorrect.") << std::endl;
+		std::cout << (output == expectedOutput ? "Correct!" : "Incorrect.") << std::endl;
 
-		//std::cout << (output == expectedOutput ? "Correct!" : "Incorrect.") << std::endl;
-		
 		// prints the test case to the console
-		//output.print();
+		output.print();
+		expectedOutput.print();
 	}
+	std::cout << correct << " / " << testingDataLabels.size() << " = " << ((float)correct / (float)testingDataLabels.size()) * 100 << "%" << std::endl;
 
-	std::cout << ((float)correct / (float)testingData.size()) * 100 << "%" << std::endl;
-*/
 	// pause
 	std::string in;
 	std::cin >> in;
 
 	return 0;
 }
+	
